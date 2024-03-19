684410708d47b7f82dce819d45cfada705465584
Ahmad Ruslandia Papua <ruslandiaahmad@gmail.com>
2024-03-20 03:09:49 +0800

    update

--- src/components/Link/Link.stories.js
+++ src/components/Link/Link.stories.js
@@ -7,8 +7,8 @@ export default {
 
 export const Default = () => (
   <StoryContainer style={{ fontSize: 18 }}>
-    <Link href="https://azure.ahmadruslandia.com">Primary link</Link>
-    <Link secondary href="https://azure.ahmadruslandia.com">
+    <Link href="https://ahmadruslandia.netlify.app">Primary link</Link>
+    <Link secondary href="https://ahmadruslandia.netlify.app">
       Secondary link
     </Link>
   </StoryContainer>
